8acd5aeb36fe2008ed5e2d7e6bbd279f14a5fd2c
Luca <memeoluca6@gmail.com>
2021-04-20 11:35:39 +0200

    added background image to .hero

--- src/script.js
+++ src/script.js
@@ -64,7 +64,7 @@ const app = new Vue ({
             {
                 images: {
                     smallImg:"",
-                    bigImg:"single-post-img3-1200x790",
+                    bigImg:"img/single-post-img3-1200x790.jpg",
                 },
                 articleInfos: {
                     author: "admin",
